2e82cdc027054594f4c5dacbc92ac408a900c19b
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-17 08:36:35 +0200

    Threat.py

---
+++ domain/entities/Threat.py
@@ -0,0 +1,8 @@
+class Threat:
+    def __init__(self, id, name, type, severity):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.severity = severity
+
+    # Getters and Setters
